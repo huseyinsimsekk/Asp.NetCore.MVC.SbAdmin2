dd67a9e7b6f6b3e6e10463037675af42e3e56c63
huseyinsimsekk <hsynsimsek723@gmail.com>
2020-12-08 22:52:05 +0300

    EmployeeController init

---
+++ src/SbAdmin2.Web/Controllers/EmployeeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SbAdmin2.Core.Contracts;
+
+namespace SbAdmin2.Web.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly IEmployeeService _employeeService;
+        public EmployeeController(IEmployeeService employeeService)
+        {
+            _employeeService = employeeService;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var employees = await _employeeService.GetAllAsync();
+            return View(employees);
+        }
+    }
+}
