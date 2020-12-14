f89dfe3822d7aefbbbd9459ca0bd4ce4430affed
huseyinsimsekk <hsynsimsek723@gmail.com>
2020-12-14 18:19:58 +0300

    SettingsController added and make selectable themecolor

---
+++ src/SbAdmin2.Core/Enumerations/Color.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SbAdmin2.Core.Enumerations
+{
+    public enum Color
+    {
+        primary=1,
+        secondary,
+        danger,
+        warning
+    }
+}
