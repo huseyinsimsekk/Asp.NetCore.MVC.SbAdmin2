8cef98b48e557f4a890b9c8e89710bc862f4591e
huseyinsimsekk <hsynsimsek723@gmail.com>
2020-12-14 22:55:56 +0300

    MainController added. Initial settings will set in there

---
+++ src/SbAdmin2.Web/Controllers/MainController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace SbAdmin2.Web.Controllers
+{
+    public abstract class MainController : Controller
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            // Assign default and change when user select another color in settings. !!! Look at this
+            if (ViewBag.ThemeColor == null)
+            {
+                ViewBag.ThemeColor = "bg-gradient-primary";
+            }
+        }
+    }
+}
