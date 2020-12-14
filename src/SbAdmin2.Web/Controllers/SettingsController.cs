66cc5847b7140444c3b463943408bc631f755075
huseyinsimsekk <hsynsimsek723@gmail.com>
2020-12-14 22:56:24 +0300

    SaveThemeColor in setting

--- src/SbAdmin2.Web/Controllers/SettingsController.cs
+++ src/SbAdmin2.Web/Controllers/SettingsController.cs
@@ -6,11 +6,17 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace SbAdmin2.Web.Controllers
 {
-    public class SettingsController : Controller
+    public class SettingsController : MainController
     {
         public IActionResult Index()
         {
             return View();
+        }
+        [HttpPost]
+        public JsonResult SaveThemeColor(string themeColor)
+        {
+            ViewBag.ThemeColor = themeColor ?? "bg-gradient-primary";
+            return Json(new { Msg = "success" });
         }
     }
 }
